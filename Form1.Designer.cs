6902107602d57e286cccaec5af6ee09a46e0556d
Karina Bashirova <72724413+karinabashirova@users.noreply.github.com>
2020-10-12 11:45:12 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace test_task_c_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.highlight_off = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.highlight_on = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rnd_btn = new System.Windows.Forms.Button();
+            this.draw_btn = new System.Windows.Forms.Button();
+            this.save_txt = new System.Windows.Forms.Button();
+            this.open_txt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // highlight_off
+            // 
+            this.highlight_off.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.highlight_off.AutoSize = true;
+            this.highlight_off.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.highlight_off.Location = new System.Drawing.Point(670, 400);
+            this.highlight_off.Name = "highlight_off";
+            this.highlight_off.Size = new System.Drawing.Size(190, 50);
+            this.highlight_off.TabIndex = 1;
+            this.highlight_off.Text = "Выключить подсветку";
+            this.highlight_off.UseVisualStyleBackColor = false;
+            this.highlight_off.Click += new System.EventHandler(this.highlight_off_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(640, 590);
+            this.flowLayoutPanel1.TabIndex = 2;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.flowLayoutPanel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.flowLayoutPanel1_MouseClick);
+            // 
+            // highlight_on
+            // 
+            this.highlight_on.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.highlight_on.Location = new System.Drawing.Point(670, 456);
+            this.highlight_on.Name = "highlight_on";
+            this.highlight_on.Size = new System.Drawing.Size(190, 44);
+            this.highlight_on.TabIndex = 3;
+            this.highlight_on.Text = "Включить подсветку";
+            this.highlight_on.UseVisualStyleBackColor = false;
+            this.highlight_on.Click += new System.EventHandler(this.highlight_on_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Location = new System.Drawing.Point(658, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 153);
+            this.label1.TabIndex = 4;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // rnd_btn
+            // 
+            this.rnd_btn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.rnd_btn.Location = new System.Drawing.Point(670, 290);
+            this.rnd_btn.Name = "rnd_btn";
+            this.rnd_btn.Size = new System.Drawing.Size(190, 47);
+            this.rnd_btn.TabIndex = 5;
+            this.rnd_btn.Text = "Нарисовать рандомные отрезки";
+            this.rnd_btn.UseVisualStyleBackColor = false;
+            this.rnd_btn.Click += new System.EventHandler(this.rnd_btn_Click);
+            // 
+            // draw_btn
+            // 
+            this.draw_btn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.draw_btn.Location = new System.Drawing.Point(670, 343);
+            this.draw_btn.Name = "draw_btn";
+            this.draw_btn.Size = new System.Drawing.Size(190, 51);
+            this.draw_btn.TabIndex = 6;
+            this.draw_btn.Text = "Нарисовать новые отрезки вручную";
+            this.draw_btn.UseVisualStyleBackColor = false;
+            this.draw_btn.Click += new System.EventHandler(this.draw_btn_Click);
+            // 
+            // save_txt
+            // 
+            this.save_txt.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.save_txt.Location = new System.Drawing.Point(670, 506);
+            this.save_txt.Name = "save_txt";
+            this.save_txt.Size = new System.Drawing.Size(190, 45);
+            this.save_txt.TabIndex = 7;
+            this.save_txt.Text = "Сохранить координаты в файл";
+            this.save_txt.UseVisualStyleBackColor = false;
+            this.save_txt.Click += new System.EventHandler(this.save_txt_Click);
+            // 
+            // open_txt
+            // 
+            this.open_txt.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.open_txt.Location = new System.Drawing.Point(670, 557);
+            this.open_txt.Name = "open_txt";
+            this.open_txt.Size = new System.Drawing.Size(190, 45);
+            this.open_txt.TabIndex = 8;
+            this.open_txt.Text = "Считать из файла";
+            this.open_txt.UseVisualStyleBackColor = false;
+            this.open_txt.Click += new System.EventHandler(this.open_txt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 653);
+            this.Controls.Add(this.open_txt);
+            this.Controls.Add(this.save_txt);
+            this.Controls.Add(this.draw_btn);
+            this.Controls.Add(this.rnd_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.highlight_on);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.highlight_off);
+            this.MaximumSize = new System.Drawing.Size(900, 700);
+            this.MinimumSize = new System.Drawing.Size(900, 700);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button highlight_off;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button highlight_on;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button rnd_btn;
+        private System.Windows.Forms.Button draw_btn;
+        private System.Windows.Forms.Button save_txt;
+        private System.Windows.Forms.Button open_txt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace test_task_c_sharp
+{
+
+    public partial class Form1 : Form
+    {
+        bool light_mode = true;  // режим подсветки, по умолчанию включен
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+
+        lines_calculation lines = new lines_calculation();
+        private void flowLayoutPanel1_MouseClick(object sender, MouseEventArgs e) // записываем координаты кликов мышкой
+        {
+            lines.points.Add(new PointF(e.X, e.Y));  // храним координаты кликов мышкой
+            if (lines.points.Count > 1)  // если количество точек достаточно, отрисовываем линии
+            {
+                lines.start_point = lines.points.Count - 1;
+                lines.checking_highlighting();  // проверяем, подсвечивать ли отрезок
+                this.Refresh();  // обновляем отрисовку формы
+            }
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            Pen pen_rect = new Pen(Color.Blue, 2);// цвет ограничивающего прямоугольника
+            Pen pen_normal_line = new Pen(Color.Black, 1);// цвет обычной линии и ее ширина
+            Pen pen_highlighted_line = new Pen(Color.Red, 3);// цвет подсвеченной линии и ее ширина
+            int side_size = 35;  // длина и ширина боковых отступов от ограничивающего прямоугольника
+            int field_size = 400;  // длина и ширина граничивающего прямоугольника
+            int side_field = side_size + field_size; // длина одной боковой части + длина прямоугольника
+            gr.DrawRectangle(pen_rect, side_size, side_size, field_size, field_size);  // нарисовали ограничивающий прямоугольник
+            PointF[] points_array = lines.points.ToArray();
+
+            if (points_array.Length > 1)  //если кликнуто как минимум 2 точки, соединяем их
+            {
+                if (light_mode == true)  // если включен режим подсветки
+                {
+                    gr.DrawLines(pen_normal_line, points_array);  // нарисовали все обычные линии
+                    for (int i = 1; i < lines.points.Count; i++)
+                    {
+                        if (lines.check_lights[i-1] == true)
+                            gr.DrawLine(pen_highlighted_line, lines.points[i - 1], lines.points[i]);
+                    }
+                }
+                else // если режим без подсветки, нарисовали обычные линии
+                    gr.DrawLines(pen_normal_line, points_array);
+            }
+            gr.Dispose();// освобождаем все ресурсы, связанные с отрисовкой
+        }
+
+        private void highlight_off_Click(object sender, EventArgs e)  // кнопка выключения режима подсветки
+        {
+            light_mode = false;
+            this.Refresh();
+            highlight_on.Enabled = true;  // включили кнопку включения подсветки
+            highlight_off.Enabled = false;  // выключили кнопку выключения подсветки
+        }
+
+        private void highlight_on_Click(object sender, EventArgs e)  // кнопка включения режима подсветки
+        {
+            light_mode = true;
+            this.Refresh();
+            highlight_off.Enabled = true;  // включили кнопку выключения подсветки
+            highlight_on.Enabled = false;  // выключили кнопку включения подсветки
+        }
+
+        private void draw_btn_Click(object sender, EventArgs e)  // рисуем отрезки вручную, старые сбрасываются
+        {
+            lines.points.Clear();  // сбросили старые значения координат
+            lines.check_lights.Clear();
+            this.Refresh();  // обновили форму
+        }
+
+        private void rnd_btn_Click(object sender, EventArgs e)  // рисуем рандомные отрезки
+        {
+            lines.points.Clear();  // сбросили старые значения координат
+            lines.check_lights.Clear();
+            lines.rnd_lines();  // заполнение списка рандомными координатами отрезков
+            lines.start_point = 1;
+            lines.checking_highlighting();
+            this.Refresh();  // обновили форму
+        }
+
+        private void save_txt_Click(object sender, EventArgs e)  // сохранили массив в файл
+        {
+            if (lines.points.Count > 1)
+            {
+                StreamWriter str = new StreamWriter("coord.txt");
+                string coord;
+                for (int i = 0; i < lines.points.Count - 1; i++)
+                {
+                    coord = lines.points[i].X.ToString() + " " + lines.points[i].Y.ToString() + " " + lines.check_lights[i];
+                    str.WriteLine(coord);
+                }
+                coord = lines.points[lines.points.Count - 1].X.ToString() + " " + lines.points[lines.points.Count - 1].Y.ToString();
+                str.WriteLine(coord);
+
+                str.Close();
+            }
+        }
+
+        private void open_txt_Click(object sender, EventArgs e)  // считали массив из файла
+        {
+            lines.points.Clear();
+            lines.check_lights.Clear();
+            StreamReader sr = new StreamReader("coord.txt");
+            string line;
+            int x, y;
+            bool c;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] coord = line.Split(new char[] { ' ' });
+                x = Convert.ToInt32(coord[0]);
+                y = Convert.ToInt32(coord[1]);
+                lines.points.Add(new PointF(x, y));
+                if (coord.Length == 3)
+                {
+                    c = Convert.ToBoolean(coord[2]);
+                    lines.check_lights.Add(c);
+                }
+                Console.WriteLine(line);
+            }
+            sr.Close();
+            this.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            highlight_on.Enabled = false;  // при загрузке по умолчанию подсветка включена, кнопка включения заблокирована
+        }
+    }
+
+    
+    public partial class lines_calculation
+    {
+        public int lines_count;  // число линий для рандомного заполнения
+        public int full_size; // полная длина/ширина поля отрисовки
+        public List<PointF> points = new List<PointF> { };  // список для хранения коррдинат кликов мышкой (будущие линии)
+        public List<bool> check_lights = new List<bool> { };  // список для хранения показателей, нужно ли подсвечивать линию
+        public int start_point;  // стартовая точка, с которой продолжаем отрисовку
+        public lines_calculation() { full_size = 470; lines_count = 15; }
+
+        public void rnd_lines()  // функция заполнения списка случайными координатами
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < lines_count - 1; i++) // задали рандомные значения для координат
+            {
+                int x = rnd.Next(0, full_size);
+                int y = rnd.Next(0, full_size);
+                points.Add(new PointF(x, y));
+            }
+            points.Add(new PointF(points[0].X, points[0].Y)); // замкнули линию
+        }
+
+        public void checking_highlighting()  // функция проверки, нужно ли подсвечивать линию
+        {
+            double tan_alpha_prev, tan_alpha_new, tan_beta_prev, tan_beta_new;
+            int side_size = 35;  // длина и ширина боковых отступов от ограничивающего прямоугольника
+            int field_size = 400;  // длина и ширина граничивающего прямоугольника
+            int side_field = side_size + field_size; // длина одной боковой части + длина прямоугольника
+            if (points.Count > 1)  //если кликнуто как минимум 2 точки, соединяем их
+            {
+                for (int i = start_point; i < points.Count; i++)
+                {
+                    // если одна точка из двух соедняемых прямой лежит внутри огр. прямоугольника, подсвечиваем прямую
+                    if ((points[i - 1].X >= side_size && points[i - 1].Y >= side_size && points[i - 1].X <= side_field && points[i - 1].Y <= side_field) ||
+                    (points[i].X >= side_size && points[i].Y >= side_size && points[i].X <= side_field && points[i].Y <= side_field))
+                        check_lights.Add(true);
+
+                    // если линии идут крестом через квадрат (из угла в угол)
+                    else if ((points[i - 1].X < side_size && points[i - 1].Y < side_size && points[i].X > side_field && points[i].Y > side_field)) // из лев. верх. в прав. нижн
+                        check_lights.Add(true);
+                    else if (points[i - 1].X > side_field && points[i - 1].Y < side_size && points[i].X < side_size && points[i].Y > side_field)  //  из прав. верх. в лев. нижн.
+                        check_lights.Add(true);
+                    else if (points[i - 1].X > side_field && points[i - 1].Y > side_field && points[i].X < side_size && points[i].Y < side_size)  // из прав. ниж. в лев. верх
+                        check_lights.Add(true);
+                    else if (points[i - 1].X < side_size && points[i - 1].Y > side_field && points[i].X > side_field && points[i].Y < side_size)  // из лев. ниж. в прав. верх.
+                        check_lights.Add(true);
+
+                    // иначе проверяем, проходит ли прямая через прямоугольник
+                    else if (points[i - 1].X < side_size && points[i - 1].Y > side_size && points[i - 1].Y < side_field && points[i].X > side_size)  // левая сторона
+                    {
+                        //посчитали тангенсы угла наклона прямой, если лежат в нужных пределах, подсвечиваем
+                        if (points[i - 1].Y > points[i].Y) // если старая точка лежит ниже новой
+                        {
+                            tan_alpha_prev = (points[i - 1].Y - side_size) / (side_size - points[i - 1].X);  // тангенс угла наклона прямой, проходящей через начальную точку прямой и верхний угол прямоугольника
+                            tan_alpha_new = (points[i - 1].Y - points[i].Y) / (points[i].X - points[i - 1].X);  // тангенс угла наклона отрисовываемой прямой
+                            if ((tan_alpha_new) < (tan_alpha_prev))
+                                check_lights.Add(true);
+                            else
+                                check_lights.Add(false);
+                        }
+                        else // если старая точка лежит выше новой
+                        {
+                            tan_beta_prev = (side_field - points[i - 1].Y) / (side_size - points[i - 1].X);  // тангенс угла наклона прямой, проходящей через начальную точку прямой и нижний угол прямоугольника
+                            tan_beta_new = (points[i].Y - points[i - 1].Y) / (points[i].X - points[i - 1].X);  // тангенс угла наклона отрисовываемой прямой
+                            if ((tan_beta_new) < (tan_beta_prev))
+                                check_lights.Add(true);
+                            else
+                                check_lights.Add(false);
+                        }
+                    }
+                    else if (points[i - 1].X > side_field && points[i - 1].Y > side_size && points[i - 1].Y < side_field && points[i].X < side_field)  // правая сторона
+                    {
+                        //посчитали тангенсы угла наклона прямой, если лежат в нужных пределах, подсвечиваем
+                        if (points[i - 1].Y > points[i].Y) // если старая точка лежит ниже новой
+                        {
+                            tan_alpha_prev = (points[i - 1].Y - side_size) / (points[i - 1].X - side_field);  // тангенс угла наклона прямой, проходящей через начальную точку прямой и верхний угол прямоугольника
+                            tan_alpha_new = (points[i - 1].Y - points[i].Y) / (points[i - 1].X - points[i].X);  // тангенс угла наклона отрисовываемой прямой
+                            if ((tan_alpha_new) < (tan_alpha_prev))
+                                check_lights.Add(true);
+                            else
+                                check_lights.Add(false);
+                        }
+                        else // если старая точка лежит выше новой
+                        {
+                            tan_beta_prev = (side_field - points[i - 1].Y) / (points[i - 1].X - side_field);  // тангенс угла наклона прямой, проходящей через начальную точку прямой и нижний угол прямоугольника
+                            tan_beta_new = (points[i].Y - points[i - 1].Y) / (points[i - 1].X - points[i].X);  // тангенс угла наклона отрисовываемой прямой
+                            if ((tan_beta_new) < (tan_beta_prev))
+                                check_lights.Add(true);
+                            else
+                                check_lights.Add(false);
+                        }
+                    }
+                    else if (points[i - 1].Y < side_size && points[i - 1].X > side_size && points[i - 1].X < side_field && points[i].Y > side_size)  // верхняя сторона
+                    {
+                        //посчитали тангенсы угла наклона прямой, если лежат в нужных пределах, подсвечиваем
+                        if (points[i - 1].X > points[i].X) // новая точка лежит левее старой
+                        {
+                            tan_alpha_prev = (points[i - 1].X - side_size) / (side_size - points[i - 1].Y);  // тангенс угла наклона прямой, проходящей через начальную точку прямой и левый угол прямоугольника
+                            tan_alpha_new = (points[i - 1].X - points[i].X) / (points[i].Y - points[i - 1].Y);  // тангенс угла наклона отрисовываемой прямой
+                            if ((tan_alpha_new) < (tan_alpha_prev))
+                                check_lights.Add(true);
+                            else
+                                check_lights.Add(false);
+                        }
+                        else // новая точка лежит правее старой
+                        {
+                            tan_beta_prev = (side_field - points[i - 1].X) / (side_size - points[i - 1].Y);  // тангенс угла наклона прямой, проходящей через начальную точку прямой и правый угол прямоугольника
+                            tan_beta_new = (points[i].X - points[i - 1].X) / (points[i].Y - points[i - 1].Y);  // тангенс угла наклона отрисовываемой прямой
+
+                            if ((tan_beta_new) < (tan_beta_prev))
+                                check_lights.Add(true);
+                            else
+                                check_lights.Add(false);
+                        }
+                    }
+                    else if (points[i - 1].Y > side_field && points[i - 1].X > side_size && points[i - 1].X < side_field && points[i].Y < side_field)  // нижняя сторона
+                    {
+                        //посчитали тангенсы угла наклона прямой, если лежат в нужных пределах, подсвечиваем
+                        if (points[i - 1].X > points[i].X) // новая точка лежит левее старой
+                        {
+                            tan_alpha_prev = (points[i - 1].X - side_size) / (points[i - 1].Y - side_field);  // тангенс угла наклона прямой, проходящей через начальную точку прямой и левый угол прямоугольника
+                            tan_alpha_new = (points[i - 1].X - points[i].X) / (points[i - 1].Y - points[i].Y);  // тангенс угла наклона отрисовываемой прямой
+                            if ((tan_alpha_new) < (tan_alpha_prev))
+                                check_lights.Add(true);
+                            else
+                                check_lights.Add(false);
+                        }
+                        else // новая точка лежит правее старой
+                        {
+                            tan_beta_prev = (side_field - points[i - 1].X) / (points[i - 1].Y - side_field);  // тангенс угла наклона прямой, проходящей через начальную точку прямой и правый угол прямоугольника
+                            tan_beta_new = (points[i].X - points[i - 1].X) / (points[i - 1].Y - points[i].Y);  // тангенс угла наклона отрисовываемой прямой
+                            if ((tan_beta_new) < (tan_beta_prev))
+                                check_lights.Add(true);
+                            else
+                                check_lights.Add(false);
+                        }
+                    }
+                    else if (points[i - 1].X < side_size && points[i - 1].Y > side_field && points[i].X > side_size && points[i].Y < side_field)  // из нижнего левого кввадрата
+                        check_lights.Add(true);
+                    else if (points[i - 1].X < side_size && points[i - 1].Y < side_size && points[i].X > side_size && points[i].Y > side_size)  // из верхнего левого кввадрата
+                        check_lights.Add(true);
+                    else if (points[i - 1].X > side_field && points[i - 1].Y > side_field && points[i].X < side_field && points[i].Y < side_field)  // из нижнего правого кввадрата
+                        check_lights.Add(true);
+                    else if (points[i - 1].X > side_field && points[i - 1].Y < side_size && points[i].X < side_field && points[i].Y > side_size)  // из верхнего правого квадрата
+                        check_lights.Add(true);
+
+                    else
+                        check_lights.Add(false);  // иначе не подсвечиваем отрезок
+                }
+
+            }
+
+        }
+    }
+
+}
